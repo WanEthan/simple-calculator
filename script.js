ca38b1fad4278f76b4dd75dac277d1464754e7cb
Ethan Wan <107653672+WanEthan@users.noreply.github.com>
2023-03-26 22:21:56 -0700

    calculator(first commit)

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+const displayDiv = document.querySelector("#display");
+
+var number1 = "";
+var number2 = "";
+var operator = "";
+
+
+function press(num) {
+    number1 += num;
+    displayDiv.innerText = number1;
+}
+
+function setOP(symbol) {
+    operator = symbol;
+    number2 = number1;
+    number1 = "";
+}
+
+function clr() {
+    operator = "";
+    number1 = "";
+    number2 = "";
+    displayDiv.innerText = "0";
+}
+
+function calculate() {
+    var outcome = 0;
+    if (operator == "+") {
+        outcome = Number(number2) + Number(number1);
+    }
+
+    if (operator == "-") {
+        outcome = Number(number2) - Number(number1);
+    }
+    
+    if (operator == "*") {
+        outcome = Number(number2) * Number(number1);
+    }
+
+    if (operator == "/") {
+        outcome = Number(number2) / Number(number1);
+    }
+    number1 = "";
+    operator = "";
+    displayDiv.innerText = outcome;
+
+}
